eaebf973d501dad3ccc62e72ad1e8eb949782aaf
Jo-Shan Lee <joshanlee@gmail.com>
2019-05-18 20:32:17 +0200

    js added

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,11 @@
 alert("Hello! Welcome to my portfolio site!");
 console.log("Hello! This is my portfolio site!");
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
+}
